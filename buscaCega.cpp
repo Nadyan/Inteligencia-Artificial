adcc4a2949ef9933c12e7897cacc1af7f5b5919e
Nadyan <nadyan_sp@hotmail.com>
2017-04-17 14:33:04 -0300

    bfs init atp 2

--- buscaCega.cpp
+++ buscaCega.cpp
@@ -9,6 +9,7 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <unistd.h>
 //#include <SFML/Graphics.hpp>
 
 #define TAM 42  //terreno TAMxTAM
@@ -21,27 +22,28 @@ typedef struct{
 
 Ponto inicio, destino;
 int terreno[TAM][TAM];
-int visitados[TAM][TAM];
+char terrenoChar[TAM][TAM];
+int visitados[TAM*TAM];
+int frente = 0, cauda = 0;
+int fila[TAM];
 
 void montaCenario();
 void imprimeCenario();
 void limpaTela();
-void dfs(int i, int j);
+void bfs(int i);
 
 int main(){
 
-  for(int i = 0; i < TAM; i++){
-    for(int j = 0; j < TAM; j++){
-      visitados[i][j] = 0;
-    }
-  }
+  memset(visitados, 0, sizeof(visitados));
 
   montaCenario();
   imprimeCenario();
 
   int start1 = inicio.x, star2 = inicio.y;
 
-  dfs(start1, star2);
+  //bfs(start1, star2);
+  bfs(start1);
+
 
   /* //Parte grafica
   sf::RenderWindow window(sf::VideoMode(800, 600), "Busca Cega");
@@ -69,12 +71,23 @@ void montaCenario(){
 
     printf("Informe o ponto de inicio(#): ");
     scanf("%d %d", &inicio.x, &inicio.y);
-    printf("Informe o ponto de destino(#): ");
+    printf("Informe o ponto de destino(*): ");
     scanf("%d %d", &destino.x, &destino.y);
 
     for(i = 0; i < TAM; i++){
         for(j = 0; j < TAM; j++){
             fscanf(f, "%d", &terreno[i][j]);
+
+            switch(terreno[i][j]){
+              case 0:
+                terrenoChar[i][j] = '0'; break;
+              case 1:
+                terrenoChar[i][j] = '1'; break;
+              case 2:
+                terrenoChar[i][j] = '2'; break;
+              case 3:
+                terrenoChar[i][j] = '3'; break;
+            }
         }
     }
 }
@@ -87,32 +100,59 @@ void imprimeCenario(){
   for(i = 0; i < TAM; i++){
     for(j = 0; j < TAM; j++){
       if(i == inicio.x && j == inicio.y)
-        printf("# ");
+        printf("  ");
       else if(i == destino.x && j == destino.y)
-        printf("* ");
+        printf("  ");
       else
-        printf("%d ", terreno[i][j]);
+        printf("%c ", terrenoChar[i][j]);
     }
     printf("\n");
   }
 }
 
-void dfs(int i, int j){
+/* // bfs fail com estrutura de grafo
+void bfs(int v){
 
-  visitados[i][j] = 1;
+    visitados[v] = 1;
 
-  for(j = 0; j < TAM; j++){
-    for(j = 0; j < TAM; j++){
-      if(!visitados[i][j] && terreno[i][j] != -1){
-        terreno[i][j] = 4;
-        limpaTela();
-        imprimeCenario();
-        dfs(j, i);
-      }
+    // enfila vertice v
+    fila[cauda] = v; // insere na cauda
+    cauda++;
+
+    while (cauda != frente){ // condicao de fila vazia
+        int i = 0;
+        // dequeue
+        int u = fila[frente];
+        //printf("%d ", u);
+        frente++;
+
+        // checagem de vizinhos de u
+        for (i = 0; i < TAM; i++){
+            // se tem vizinho
+            if (!visitados[i] && (terreno[u][i] != -1)){
+                fila[cauda] = i;
+                cauda++;
+
+                visitados[i] = 1;
+                terrenoChar[u][i] = '*';
+
+                imprimeCenario();
+                usleep(1000*20);
+                limpaTela();
+
+                if(u == destino.x && i == destino.y)
+                    break;
+            }
+        }
     }
-  }
+}*/
+
+// bfs segunda tentativa sem estrutura de grafo
+void bfs(int i, int j){
+
+
 }
 
 void limpaTela(){
-    system("cls || clear");
+    system("clear");
 }
