67e21a774e228d7eb46c93e70a6df934859a46a1
Nadyan <nadyan_sp@hotmail.com>
2017-05-22 06:27:54 -0300

    need to fix fprintf

--- simulated_annealing/sat_sa.c
+++ simulated_annealing/sat_sa.c
@@ -8,18 +8,20 @@
     Na análise, verificar se o algoritmo convergiu,
     ou seja, o espaço de busca se afunila com o tempo,
     diferente da busca aleatoria
+
+    gcc -o sat_sa sat_sa.c -lm
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 /* Defines */
-#define T0                  500000       // Temperatura inicial
-#define TN                  0            // Temperatura final
+#define T0                  4.0          // Temperatura inicial
+#define TN                  0.0          // Temperatura final
 #define N                   500000       // Quantidade de iterações
 #define CHANCE_PERTURBACAO  3            // % de chance de bit flip da solucao
-#define INSTANCIA           1
 
 /* instancia 1 */
 #define QTDVARS             20          // Quantidade de variaveis
@@ -36,9 +38,10 @@
 /* Funcoes */
 void monta_vetores();
 void gera_solucao_inicial();
-int resfriamento(int i);                
+float resfriamento(int i);                
 int avalia_solucao();			// fitness
 void perturba();				// crossover
+void avalia_print();
 
 /* Vars */
 int tam = 3 * QTDCLAUS;
@@ -47,12 +50,12 @@ int negs[3 * QTDCLAUS];
 int solucao[QTDVARS];
 int candidato_sol[QTDVARS];
 int qtdAnterior = 0;
-int delta;
+float delta;
 
 int main(){
-    int j, k, aux;
-    int temperatura = T0;
-    float chance;
+    int j, k, aux, oi = 0, oi2 = 0;
+    float temperatura = T0;
+    float chance, exp;
 
     srand((unsigned)time(NULL));
     
@@ -62,32 +65,47 @@ int main(){
     for(j = 0; j < N; j++){
       
         aux = avalia_solucao();                 // avalia a solucao contida no candidato_sol[]
-        temperatura = resfriamento(j);
-        
-        if(aux == 1){                           // se a solucao candidata for melhor
-            for(k = 0; k < tam; k++)
-                solucao[k] = candidato_sol[k];  // atribui como solucao
+        //printf("%d\n", aux);
+
+              
+        if(aux == 0){                     // se a solucao canditada for pior
             
-        }else if(aux == 0){                     // se a solucao canditada for pior
-            
-            chance = rand() % 100;
+            chance = ((float)rand()/(float)(RAND_MAX)) * 100; // gera um float aleatorio
             //printf("%f\n", chance);
+
+            exp = delta/temperatura;
+            //printf("%f\n", exp);
             
-            if(chance < (3^(-delta/temperatura))*100){           // se der, atribui a solucao pior
-            //if(chance < temperatura){
+            //printf("%f\n", (pow(2.7, exp)*100.0));
+
+            if(chance < (pow(2.7, exp)*100.0)){
                 for(k = 0; k < tam; k++)
                     solucao[k] = candidato_sol[k];
+
+                oi++;
+                //avalia_print();
             }
+        }else if(aux == 1){                           // se a solucao candidata for melhor
+            for(k = 0; k < tam; k++)
+                solucao[k] = candidato_sol[k];  // atribui como solucao
+
+            oi2++;
+            //avalia_print();
         }
 
         perturba();
+        temperatura = resfriamento(j);
     }
+
+    printf("piorias: %d\nmelhorias: %d\n", oi, oi2);    
 
     return 0;
 }
 
 void monta_vetores(){
     
+    int ignore;
+
     /* instancia 1 */
     FILE *f = fopen("instancias/uf20_01.txt", "r");
     /* instancia 2 */
@@ -115,6 +133,13 @@ void monta_vetores(){
 
             vet[i] = abs(vet[i]);           // armazena apenas a variavel no vetor vet, e nao se está negada ou nao
                                             // a negacao fica armazenada no vetor negs
+
+            ignore++;
+
+            if(ignore == 3){
+                fscanf(f, "%d", &ignore); 
+                //printf("%d\n", ignore);
+            }
         }
     }
 }
@@ -162,10 +187,8 @@ void perturba(){
 }
 
 int avalia_solucao(){
-    int qtdSatisfeitas = 0, n; // var contadora de clausulas satisfeitas
+    int qtdSatisfeitas = 0, n, aux; // var contadora de clausulas satisfeitas
     int var1, var2, var3;
-
-    //FILE *escreve = fopen("resultados.txt", "a+");
             
     // 3-CNF -> (A || B || C) && (D || E || F) && ...
     for(n = 0; n < tam; n += 3){
@@ -196,27 +219,74 @@ int avalia_solucao(){
             qtdSatisfeitas++;
     }
     
-    printf("%d\n", qtdSatisfeitas);
-    //fprintf(escreve, "%d\n" , qtdSatisfeitas);
+    //printf("%d\n", qtdSatisfeitas);
 
-    delta = qtdSatisfeitas - qtdAnterior;
+    delta = (float)(qtdSatisfeitas - qtdAnterior);
+
+    aux = qtdAnterior;
+    qtdAnterior = qtdSatisfeitas;
     
-    if(qtdSatisfeitas >= qtdAnterior){
-        qtdAnterior = qtdSatisfeitas;
-        //printf("%d\n", qtdSatisfeitas);
+    if(qtdSatisfeitas >= aux){
         return 1;
-    }else
+    }else{
         return 0;
-
+    }
 }
 
-int resfriamento(int i){
-    int novaTemp;
+void avalia_print(){
+    /* Funcao utilziada para imprimir a quantidade
+       de clausulas satisfeitas pela solucao escolhida */
+
+    int var1, var2, var3, qtdSatisfeitas = 0, n;
+
+    FILE *escreve = fopen("resultados.txt", "a+");
+            
+    // 3-CNF -> (A || B || C) && (D || E || F) && ...
+    for(n = 0; n < tam; n += 3){
+        var1 = solucao[vet[n]];
+        var2 = solucao[vet[n+1]];
+        var3 = solucao[vet[n+2]];
+        
+        if(negs[n] == 1){
+            if(var1 == 1)
+                var1 = 0;
+            else if(var1 == 0)
+                var1 = 1;
+        }
+        if(negs[n+1] == 1){
+            if(var2 == 1)
+                var2 = 0;
+            else if(var2 == 0)
+                var2 = 1;
+        }
+        if(negs[n+2] == 1){
+            if(var3 == 1)
+                var3 = 0;
+            else if(var3 == 0)
+                var3 = 1;
+        }
+        
+        if(var1 || var2 || var3)
+            qtdSatisfeitas++;
+    }
+
+    fprintf(escreve, "%d\n", qtdSatisfeitas);
+    printf("%d\n", qtdSatisfeitas);
+}
+
+float resfriamento(int i){
+    float novaTemp;
         
     /* Fórmula de resfriamento linear */
-    novaTemp = T0 - i * ((T0 - TN) / N);
+    novaTemp = T0 - (float)i * ((T0 - TN) / (float)N);
 
-    //printf("%d\n", novaTemp);
+    /* Fórmula de resfriamento n5 */
+    //novaTemp = ((T0 - TN)/(1.0 + pow(2.7, 3.0*((float)i - ((float)N)/2.0))));
+
+    /* busca aleatoria */
+    //novaTemp = 100.0;
+
+    //printf("%f\n", novaTemp);
         
     return novaTemp;
 }
